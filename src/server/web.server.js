734f7356e085194d43c173c36a109274805c272f
Dave Dehghani <davedehghani@gmail.com>
2018-08-19 12:50:41 -0400

    created webpack configuration file with two entry points (one for web app, other for the web server

--- src/server/web.server.js
+++ src/server/web.server.js
@@ -9,7 +9,7 @@ export default class WebServer {
   start() {
     return new Promise((resolve, reject) => {
       try {
-        this.server = this.app.listen(3001, function() {
+        this.server = this.app.listen(3000, function() {
           resolve();
         })
       } catch (e) {
